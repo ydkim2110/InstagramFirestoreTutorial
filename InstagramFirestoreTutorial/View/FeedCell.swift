cd1abbc75385a86ab19d0b279398c5d83ed5f887
Kim Yongdae <ydkim2110@gmail.com>
2021-04-02 10:14:19 +0900

    Commit Test

--- InstagramFirestoreTutorial/View/FeedCell.swift
+++ InstagramFirestoreTutorial/View/FeedCell.swift
@@ -10,6 +10,7 @@ import UIKit
 class FeedCell: UICollectionViewCell {
     
     // MARK: - Properties
+    
     private let profileImageView: UIImageView = {
         let iv = UIImageView()
         iv.contentMode = .scaleAspectFill
